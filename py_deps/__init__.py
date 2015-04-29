eae9d165b676829453cd8a3f2a21cbf1517478ac
Kouhei Maeda <mkouhei@palmtb.net>
2015-04-29 19:28:17 +0900

    Simplify the import method of module.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- py_deps/__init__.py
+++ py_deps/__init__.py
@@ -1,1 +1,15 @@
-"""py-deps provides parsing the Python deps and generating graph data."""
+"""py-deps provides parsing the Python deps and generating graph data.
+
+Initialize.
+-----------
+
+::
+    $ python
+    >>> from py_deps import Package
+    >>> pkg = Package('py-deps')
+    >>> pkg.download()
+    >>> pkg.list_requires()
+    >>> pkg.cleanup()
+
+"""
+from py_deps.deps import Package  # silence pyflakes
