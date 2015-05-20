0c08ab2ad60530b5e7674db74d90c0b08c78ad78
Kouhei Maeda <mkouhei@palmtb.net>
2015-05-20 17:41:10 +0900

    Bump version.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ with open('extras_requirement.txt', 'w') as _file:
     _file.write('\n'.join(extras_require.get('reST')))
 
 setup(name='py-deps',
-      version='0.3.0',
+      version='0.4.0',
       description='parsing the Python deps and generating graph data',
       long_description=long_description,
       author='Kouhei Maeda',
